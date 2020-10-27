8687bec080745510388c9d4b63110e6bdb882a53
Jan Forgac <jan.forgac@educanet.cz>
2020-10-27 20:08:30 +0100

    jump animations added

--- ProjectFGT/KinematicBody2D.gd
+++ ProjectFGT/KinematicBody2D.gd
@@ -2,60 +2,70 @@ extends KinematicBody2D
 
 var motion = Vector2(0,0)
 var movementPhase: String
-const PLEYERSPEED = 350
+const PLEYERSPEED = 533
 var speed1 = PLEYERSPEED
 const GRAVITY = 30
-const JUMPFORCE = -865
+const JUMPFORCE = -1153
 var fall = GRAVITY
+var x = 1
 
+var currentlyPlaying = null
 
 func _physics_process(_delta):
-	
 	if Input.is_action_pressed("right"):
 		$Sprite.flip_h = false
 		_right_left_movement(speed1)
 	elif Input.is_action_pressed("left"):
 		$Sprite.flip_h = true
 		_right_left_movement(-speed1)
-	else:
-		$Sprite.play("idle")
+	elif is_on_floor():
 		movementPhase = "idle"
+		_play("idle")
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		motion.y = JUMPFORCE
 	_fall_physics()
-	if !is_on_floor():
-		$Sprite.play("air")
 	motion = move_and_slide(motion, Vector2.UP)
 	motion.x = lerp(motion.x,0,1)
+	
 
 func _right_left_movement(rychlost):
 		if is_on_floor():
 			if Input.is_action_pressed("sprint"):
-				$Sprite.play("sprint")
-				rychlost = 1.75 * rychlost
-				print(motion.x)
+				rychlost = 1.53 * rychlost
 				movementPhase = "sprint"
+				_play("sprint")
 			else:
 				rychlost = rychlost
 				movementPhase = "walk"
-				$Sprite.play("walk")
+				_play("walk")
 		else:
 			match movementPhase:
 					"sprint":
-						rychlost = rychlost * 1.85
+						rychlost = rychlost * 1.83
 					"walk":
 						rychlost = rychlost
 					"idle":
-						rychlost = rychlost * 0.7
+						rychlost = rychlost * 0.8
 		motion.x = rychlost
 
 func _fall_physics():
-			if fall > 45:
-				fall = 45
+			if fall > 53:
+				fall = 53
 			fall += fall/10
 			motion.y += fall
 			if is_on_floor():
-				speed1 = PLEYERSPEED
-				fall = 35
-			else:
-				speed1 = 1.3 * PLEYERSPEED
+				fall = 40
+			else: 
+				print(motion.y)
+				print(currentlyPlaying)
+				if motion.y > JUMPFORCE/2 && motion.y < -JUMPFORCE/2 :
+					_play("airTop")
+				elif motion.y < JUMPFORCE/2:
+					_play("airUp")
+				else:
+					_play("airDown")
+
+func _play(animName):
+	currentlyPlaying = animName
+	$Sprite.play(animName)
+	
