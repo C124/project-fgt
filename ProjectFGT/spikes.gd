4774724b6377c4b361bc5159d6cc006d29929929
Jan Forgac <jan.forgac@educanet.cz>
2020-11-20 10:48:11 +0100

    unused cleaned

--- ProjectFGT/spikes.gd
+++ ProjectFGT/spikes.gd
@@ -14,6 +14,3 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
-func _on_Area2D_body_entered(body):
-	if body.is_in_group("player"):
-		get_tree().reload_current_scene()
