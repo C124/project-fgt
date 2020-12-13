a10db04cc282e45926a83bd0c9d019f99878f741
Nick <43199836+Nik-Streicher@users.noreply.github.com>
2020-12-13 17:18:18 +0100

    Add files via upload
    přidány textury + ničení projektilu o objekty

--- ProjectFGT/turret/projectile.gd
+++ ProjectFGT/turret/projectile.gd
@@ -3,7 +3,7 @@ extends KinematicBody2D
 var velocity = Vector2()
 export var speed_y = -300
 export var speed_x = 0
-export var point_of_detroy = 700
+#export var point_of_detroy = 700
 
 func _ready():
 	print("Shooting projectile")
@@ -13,6 +13,14 @@ func _ready():
 
 
 func _physics_process(delta):
-	if position.y < -point_of_detroy:
-		queue_free()
+#	if position.y < -point_of_detroy:
+#		queue_free()
 	move_and_slide(velocity)
+	
+	for index in get_slide_count():
+		var collision = get_slide_collision(index)
+		
+		# pro přidání dalšího objektu interakce použijte daní formát -> || is název_objektu
+		if collision.collider is KinematicBody2D || collision.collider is TileMap:
+			queue_free()
+			
